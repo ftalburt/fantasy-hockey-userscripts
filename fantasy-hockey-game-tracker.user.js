05dd23e1736dd6db3077eb792f6f5a63cc112080
Forrest Talburt <2643911+ftalburt@users.noreply.github.com>
2022-01-08 21:07:52 -0600

    Bump version

--- fantasy-hockey-game-tracker.user.js
+++ fantasy-hockey-game-tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fantasy Hockey Game Tracker
 // @namespace    http://ftalburt.com/
-// @version      0.7.0
+// @version      0.8.0
 // @description  Adds information about number of games left to boxscore page on ESPN fantasy hockey
 // @author       Forrest Talburt
 // @match        https://fantasy.espn.com/hockey/boxscore*
